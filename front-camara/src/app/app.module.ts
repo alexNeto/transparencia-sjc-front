44227ef90e2220389b9f3c85b57bfe4aeb89dceb
alexNeto <alex.neto3@hotmail.com>
2017-10-16 16:31:07 -0200

    tentando de novo

--- front-camara/src/app/app.module.ts
+++ front-camara/src/app/app.module.ts
@@ -2,13 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TodosdadosComponent } from './todosdados/todosdados.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodosdadosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
